627259021bdebb927f51c25dc907b47670525745
Lisandro Betancur <lbetagu@gmail.com>
2023-12-06 15:27:58 -0500

    new feature about read csv

---
+++ src/main/java/co/com/auto/utils/CsvUtil.java
@@ -0,0 +1,64 @@
+package co.com.auto.utils;
+
+import com.fasterxml.jackson.dataformat.csv.CsvMapper;
+import com.fasterxml.jackson.dataformat.csv.CsvSchema;
+import org.slf4j.LoggerFactory;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+public final class CsvUtil {
+  private static final String FILTER_ID = "filterId";
+
+  private static final org.slf4j.Logger LOGGER = LoggerFactory.getLogger(CsvUtil.class);
+  private CsvUtil() {}
+
+  public static List<Map<String, String>> getAllDataCsv(String csvName) {
+    List<Map<String, String>> dataList = new ArrayList<>();
+    try{
+      File file =
+              new File("./src/test/resources/data/" + csvName + ".csv");
+      InputStream in = new FileInputStream(file);
+      Reader reader = new InputStreamReader(in, StandardCharsets.UTF_8);
+      Iterator<Map<String, String>> iterator =
+              new CsvMapper()
+                      .readerFor(Map.class)
+                      .with(CsvSchema.emptySchema().withColumnSeparator(',').withHeader())
+                      .readValues(reader);
+      while (iterator.hasNext()) {
+        Map<String, String> row = iterator.next();
+        dataList.add(row);
+      }
+      in.close();
+      reader.close();
+    }catch (IOException e){
+      LOGGER.error(e.getMessage());
+    }
+    return dataList;
+  }
+
+  public static List<Map<String, String>> getDataCsvWithFilter(String csvName, String filter){
+    return filterList(getAllDataCsv(csvName), filter);
+  }
+
+  public static List<Map<String, String>> filterList(List<Map<String, String>> dataTable, String filterId) {
+    List<Map<String, String>> filteredList = new ArrayList<>();
+    for (Map<String, String> list : dataTable) {
+      if (list.get(FILTER_ID).equalsIgnoreCase(filterId.toLowerCase())) {
+        filteredList.add(list);
+      }
+    }
+    for (int i = 0; i < filteredList.size(); i++) {
+      filteredList.get(i).remove(FILTER_ID);
+    }
+    return filteredList;
+  }
+
+  public static Map<String, String> getUniqueMapDataCsv(String csvName, String filter){
+    return getDataCsvWithFilter(csvName, filter).stream().findFirst().orElse(null);
+  }
+}
--- src/test/java/co/com/auto/TestRunner.java
+++ src/test/java/co/com/auto/TestRunner.java
@@ -2,13 +2,39 @@ package co.com.auto;
 
 import co.com.auto.services.Auth;
 import co.com.auto.services.Booking;
+import co.com.auto.utils.CsvUtil;
 import net.serenitybdd.junit.runners.SerenityRunner;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import java.util.List;
+import java.util.Map;
 
 @RunWith(SerenityRunner.class)
 public class TestRunner {
+
+    @Test
+    public void getAllDataCsv() {
+        List<Map<String, String>> mapList =  CsvUtil.getAllDataCsv("iyo-z-n");
+        mapList.forEach(System.out::println);
+    }
+
+    @Test
+    public void getDataCsvWithFilter() {
+        List<Map<String, String>> mapList =  CsvUtil.getDataCsvWithFilter("iyo-z-n", "active");
+        mapList.forEach(System.out::println);
+    }
+    @Test
+    public void getUniqueMapDataCsv() {
+        Map<String, String> map =  CsvUtil.getUniqueMapDataCsv("iyo-z-n", "inActive");
+        System.out.println("url: " + map.get("url"));
+        System.out.println("headerContent: " + map.get("headerContent"));
+        System.out.println("headerAuth: " + map.get("headerAuth"));
+        System.out.println("customerId: " + map.get("customerId"));
+        System.out.println("agreementId: " + map.get("agreementId"));
+        System.out.println("dateFrom: " + map.get("dateFrom"));
+        System.out.println("dateTo: " + map.get("dateTo"));
+    }
 
     @Test
     public void testLoginSuccessfully() {
